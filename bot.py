7d139fbfb49fc57c7a85bba32f89673073ee8e6e
Okhlopkov Daniil Olegovich <okhlopkov@phystech.edu>
2017-11-30 22:52:48 +0300

    add bot template

---
+++ bot.py
@@ -0,0 +1,48 @@
+import telebot
+import subprocess
+
+fradio_chat_id = -1001120092433
+path_to_photo = "/Users/fiztehradio/Camera/rubka.png"
+sooq_sticker1 = "CAADAgADIgAD1vl9CIhY2t5j3nJoAg"
+sooq_sticker2 = "CAADAgADIwAD1vl9CF9PaiN4TXvCAg"
+delay = 0
+
+def take_photo(wait=0):
+	command = "imagesnap %s" % path_to_photo
+	if wait:
+		command += " -w %f" % wait
+	proc = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+	output, error = proc.communicate()
+	if error is not None:
+		print (error)
+		return False
+	return True
+
+with open("phystechbot.token", "r") as file:
+	token = file.read().strip()
+	bot = telebot.TeleBot(token)
+
+print("Started bot with token /"+token+"/")
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
+
+@bot.message_handler(commands=['photo']) # func=lambda m: m == "photo")
+def send_photo(message):
+	if fradio_chat_id != message.chat.id:
+		bot.send_message(message.chat.id, "Вы не в чате команды Физтех.Радио. Сори :(")
+		return
+	if take_photo(wait=delay):
+		with open(path_to_photo, 'rb') as photo:
+			bot.send_photo(message.chat.id, photo)
+
+@bot.message_handler(commands=['sooq']) # func=lambda m: m == "photo")
+def send_sooqa(message):
+	bot.send_sticker(message.chat.id, sooq_sticker1)
+
+@bot.message_handler(commands=['sooqa']) # func=lambda m: m == "photo")
+def send_sooqa(message):
+	bot.send_sticker(message.chat.id, sooq_sticker2)
+
+bot.polling()
