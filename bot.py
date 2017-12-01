7f315173170a75af8b9237f8896a987eddc33974
Okhlopkov Daniil Olegovich <okhlopkov@phystech.edu>
2017-12-01 19:48:12 +0300

    add prepare func

--- bot.py
+++ bot.py
@@ -12,39 +12,48 @@ fradio_chat_id = None
 token = None
 capture = None
 
+
 def prepare():
-	with open(fizteh_chat_id_path, "r") as file:
-		fradio_chat_id = file.read().strip()
+    with open(fizteh_chat_id_path, "r") as file:
+        fradio_chat_id = file.read().strip()
 
-	with open(bot_token, "r") as file:
-		token = file.read().strip()
-		bot = telebot.TeleBot(token)
+    with open(bot_token, "r") as f:
+        token = f.read().strip()
 
-prepare()
-print("Started bot with token /"+token+"/")
-capture = Gopro()
-if not capture.ready:
-	print("Can't init Gopro")
+    bot = telebot.TeleBot(token)
+    print("Started bot with token /" + token + "/")
+
+    capture = Gopro()
+    if not capture.ready:
+        print("Can't init Gopro")
+
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+    bot.reply_to(message, "Howdy, how are you doing?")
 
-@bot.message_handler(commands=['photo']) # func=lambda m: m == "photo")
+
+@bot.message_handler(commands=['photo'])  # func=lambda m: m == "photo")
 def send_photo(message):
-	if fizteh_chat_only and fradio_chat_id != message.chat.id:
-		bot.send_message(message.chat.id, u"Вы не в чате команды Физтех.Радио. Сори :(")
-		return
-	if capture.take_photo(path_to_photo):
-		with open(path_to_photo, 'rb') as photo:
-			bot.send_photo(message.chat.id, photo)
+    if fizteh_chat_only and fradio_chat_id != message.chat.id:
+        bot.send_message(
+            message.chat.id, u"Вы не в чате команды Физтех.Радио. Сори :(")
+        return
+    if capture.take_photo(path_to_photo):
+        with open(path_to_photo, 'rb') as photo:
+            bot.send_photo(message.chat.id, photo)
 
-@bot.message_handler(commands=['sooq']) # func=lambda m: m == "photo")
+
+@bot.message_handler(commands=['sooq'])  # func=lambda m: m == "photo")
 def send_sooqa(message):
-	bot.send_sticker(message.chat.id, sooq_sticker1)
+    bot.send_sticker(message.chat.id, sooq_sticker1)
 
-@bot.message_handler(commands=['sooqa']) # func=lambda m: m == "photo")
+
+@bot.message_handler(commands=['sooqa'])  # func=lambda m: m == "photo")
 def send_sooqa(message):
-	bot.send_sticker(message.chat.id, sooq_sticker2)
+    bot.send_sticker(message.chat.id, sooq_sticker2)
 
-bot.polling()
+
+if __name__ == "__main__":
+    prepare()
+    bot.polling()
