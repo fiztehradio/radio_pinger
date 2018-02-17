2223fea2f153defa63a4f540d46450d29b466411
Dannil <danokhlopkov@gmail.com>
2018-02-17 18:17:02 +0300

    add broken_radio sticker

--- consts.py
+++ consts.py
@@ -4,6 +4,7 @@ fizteh_chat_only = False
 # stickers
 sooq_sticker1 = "CAADAgADIgAD1vl9CIhY2t5j3nJoAg"
 sooq_sticker2 = "CAADAgADIwAD1vl9CF9PaiN4TXvCAg"
+broken_radio_sticker1 = "CAADAgADFgADiyvTD77XYHl3dLA4Ag"
 
 # files
 fizteh_chat_id_path =  "data/fiztehradio.chatid"
