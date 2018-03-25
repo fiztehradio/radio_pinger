2d058f2b0ecb8d24a1734f16e6d85c45cf1b7c68
Okhlopkov Daniil Olegovich <okhlopkov@phystech.edu>
2018-03-25 15:54:52 +0300

    convert to unix service

--- bot.py
+++ bot.py
@@ -1,42 +1,64 @@
 #!/usr/bin/env python2.7
 # coding: utf8
 
+import sys
 import random
 import telegram
 
 from consts import *
 from stream_status import check_stream
 
+SLEEP_SECONDS = 60
+
+
 def reply_sticker(sticker_id):
     bot.send_sticker(chat_id=fradio_chat_id, sticker=sticker_id)
+
 
 def reply_text(text):
     bot.send_message(chat_id=fradio_chat_id, text=text)
 
+
 def reply_broken_radio():
     actions = [
-        lambda: reply_text("Походу, радио сдохло! Проверьте: http://radio.mipt.ru"),
-        lambda: reply_text("@mierokhin, радио кажись сделало сибастиена из эфира! Разберись с этим: http://radio.mipt.ru"),
-        lambda: reply_text("@Exferro, радио из дэд. Разберись с этим: http://radio.mipt.ru"),
-        lambda: reply_text("@ivanpotylitcyn, радио из дэад. Разберись с этим: http://radio.mipt.ru"),
+        lambda: reply_text(
+            "Походу, радио сдохло! Проверьте: http://radio.mipt.ru"),
+        lambda: reply_text(
+            "@mierokhin, радио кажись сделало сибастиена из эфира! Разберись с этим: http://radio.mipt.ru"),
+        lambda: reply_text(
+            "@Exferro, радио из дэд. Разберись с этим: http://radio.mipt.ru"),
+        lambda: reply_text(
+            "@ivanpotylitcyn, радио из дэад. Разберись с этим: http://radio.mipt.ru"),
         lambda: reply_sticker(broken_radio_sticker1),
-        lambda: (reply_text("не работает http://radio.mipt.ru"), reply_sticker(broken_radio_sticker2)),
-        lambda: (reply_text("радио не работает"), reply_sticker(broken_radio_sticker3)),
-        lambda: (reply_text("радио не работает :С :С :С"), reply_sticker(broken_radio_sticker2))
+        lambda: (reply_text("не работает http://radio.mipt.ru"),
+                 reply_sticker(broken_radio_sticker2)),
+        lambda: (reply_text("радио не работает"),
+                 reply_sticker(broken_radio_sticker3)),
+        lambda: (reply_text("радио не работает :С :С :С"),
+                 reply_sticker(broken_radio_sticker2))
     ]
     random.choice(actions)()
+
+
+if len(sys.argv) < 2:
+    print("Need Telegram bot token")
+    exit()
+
+token = sys.argv[1]
 
 with open(fizteh_chat_id_path, "r") as file:
     fradio_chat_id = file.read().strip()
 
-print("FIZTEH RADIO token is /"+fradio_chat_id+"/")
+print("FIZTEH RADIO token is /" + fradio_chat_id + "/")
 
-with open(bot_token, "r") as f:
-    token = f.read().strip()
+# with open(bot_token, "r") as f:
+#     token = f.read().strip()
 
 bot = telegram.Bot(token)
 print("Started bot with token /" + token + "/")
 
-is_alive = check_stream()
-if not is_alive:
-    reply_broken_radio()
+while True:
+    is_alive = check_stream()
+    if not is_alive:
+        reply_broken_radio()
+    time.sleep(SLEEP_SECONDS)
